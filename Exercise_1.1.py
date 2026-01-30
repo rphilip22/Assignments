7b823566c7582ab53d9b92bf157bffc1bfc54bed
rphilip22 <reuben.philip@student.fairfield.edu>
2026-01-30 16:57:41 -0500

    Assignment_1

--- Exercise_1.1.py
+++ Exercise_1.1.py
@@ -1,9 +1,9 @@
 revenue = float(input("Please enter the revenue amount: "))
 cost = float(input("Please enter the cost amount: "))
-profit = revenue - cost
+profit = round(revenue - cost, 2)
 margin = 0
 if revenue > 0:
-    margin = (profit/revenue) * 100
-    print
+    margin = round((profit/revenue) * 100, 2)
+    print(f"Profit = ${profit} | Margin = {margin}%")
 else:
     print("Invalid revenue")
